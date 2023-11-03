9679fb83ac52f5acb41fcb7c3c020e6e5344d9ce
Mirabror Sodikov <mirabror9545@gmail.com>
2023-11-03 15:56:06 +0500

    initial commit

---
+++ socket-service/index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from socket-service');
+});
+
+server.listen(process.env.PORT, () => {
+  console.log('Server listening on port 3000');
+});
