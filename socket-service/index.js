21be720cb3595ba3a1597d16ff094d85fdfea982
Mirabror Sodikov <mirabror9545@gmail.com>
2023-11-09 19:43:52 +0500

    socket services setup

--- socket-service/index.js
+++ socket-service/index.js
@@ -1,9 +1,24 @@
-const http = require('http');
+import http from 'http';
+import { Server } from 'socket.io';
 
-const server = http.createServer((req, res) => {
-  res.end('Hello from socket-service');
+const server = http.createServer((req, res) => res.end('OK'));
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    console.log(`Message: ${msg}`);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
 });
 
-server.listen(process.env.PORT, () => {
-  console.log('Server listening on port 3000');
+server.listen(process.env.PORT, (err) => {
+  if (err) console.error(err);
+  console.log(`Server listening on port ${process.env.PORT}`);
 });
